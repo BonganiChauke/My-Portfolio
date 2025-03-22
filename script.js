214847690107271b4562bdd05ace47e8b007a08a
BonganiChauke <bonganichauke11@outlook.com>
2025-03-22 17:19:38 +0200

    About me tab functionality for user to switch to different tabs

--- script.js
+++ script.js
@@ -40,3 +40,17 @@ resumeBtns.forEach((btn, idx) => {
   })
 
 });
+
+// About me tab functionality 
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
