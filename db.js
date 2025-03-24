61d545a4b8447a52e27ff222d60bea970a51cbab
BonganiChauke <bonganichauke11@outlook.com>
2025-03-24 15:56:10 +0200

    form event handling

--- db.js
+++ db.js
@@ -1,1 +1,20 @@
 // form event handling 
+document.getElementById("contact-form").addEventListener('click', (e) => {
+
+    //prevent default load
+    e.preventDefault();
+
+    // declaring form variables
+    const first_name = document.getElementById("first_name");
+    const last_name = document.getElementById("last_name");
+    const email = document.getElementById("email");
+    const phone_number = document.getElementById("phone_number");
+    const message = document.getElementById("message");
+
+    // ctrl value
+    const valid = false;
+
+
+    // if statement to handle inputs
+    
+});
