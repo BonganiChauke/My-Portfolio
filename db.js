df64e05afc9dff5fbb819589833c85c3ab0a195d
BonganiChauke <bonganichauke11@outlook.com>
2025-04-01 21:47:15 +0200

    In progress doing form validation

--- db.js
+++ db.js
@@ -1,44 +1,70 @@
+ // declaring form variables
+ const first_name = document.getElementById("first_name");
+ const last_name = document.getElementById("last_name");
+ const email = document.getElementById("email");
+ const phone_number = document.getElementById("phone_number");
+ const message = document.getElementById("message");
+
+
+// function to check inputs
+function checkInputs() {
+    // selecting all input elements with class "item"
+    const items = document.querySelectorAll(".item");
+
+    // looping through each element 
+    for (const item of items) {
+
+        // check if input is empty
+        if (item.value.trim() === "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        //
+        if(item[1].value != ""){
+            validateEmail();
+        }
+
+        item[1].addEventListener("keyup", () => {
+            validateEmail();
+        })
+
+        // event listener for input changes
+        item.addEventListener("keyup", () => {
+            if (item.value.trim() !== "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            } else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+
+    }
+}
+
+// function to check email validation
+function validateEmail() {
+    const items = document.getElementById("error-txttt");
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    // if 
+    if(!email.value.match(emailRegex)){
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+        items.textContent = "yoh"
+        
+    }else{
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
 // form event handling 
-
-
-
-    // function to validate user first and last names
-    // Names validation function
-    function validateNames(name) {
-        const hasNumber = /[0-9]/.test(name); // Number
-        const hasSpecialChar = /[!@@#\$%\^\&*\)\(+=._-]/.test(name); // Special character
-
-        let errorMessage = "";
-        if (name === "") errorMessage += "Name is required.\n";
-        if (name.length < 3) errorMessage += "Name cannot be less than 3 letters.\n";
-        if (hasNumber) errorMessage += "Name cannot contain numbers.\n";
-        if (hasSpecialChar) errorMessage += "Name cannot contain special characters.\n";
-
-        return errorMessage;
-    }
-
-    // Function to display error message
-    function displayErrorMessage(elementId, message) {
-        document.getElementById(elementId).textContent = message;
-    }
-
 document.getElementById("contact-form").addEventListener('submit', (e) => {
 
     //prevent default load
     e.preventDefault();
-
-    // declaring form variables
-    const first_name = document.getElementById("first_name");
-    const last_name = document.getElementById("last_name");
-    const email = document.getElementById("email");
-    const phone_number = document.getElementById("phone_number");
-    const message = document.getElementById("message");
-
-    //
-    let nameError = validateNames(first_name)
-    
-    if(nameError !== "" ){
-        displayErrorMessage("error-txtt", nameError)
-    }
+    checkInputs();
 
 });
