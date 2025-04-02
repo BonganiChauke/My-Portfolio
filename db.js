e56d6284eeb366b1198b78df21a61c22c7e4dc3e
BonganiChauke <bonganichauke11@outlook.com>
2025-04-02 10:08:13 +0200

    Added a on focus function to first name to indicate to the user if the inputs are not valid

--- db.js
+++ db.js
@@ -1,70 +1,39 @@
- // declaring form variables
- const first_name = document.getElementById("first_name");
- const last_name = document.getElementById("last_name");
- const email = document.getElementById("email");
- const phone_number = document.getElementById("phone_number");
- const message = document.getElementById("message");
+// declaring form variables
+const first_name = document.getElementById("first_name");
+const last_name = document.getElementById("last_name");
+const email = document.getElementById("email");
+const phone_number = document.getElementById("phone_number");
+const message = document.getElementById("message");
 
-
-// function to check inputs
-function checkInputs() {
-    // selecting all input elements with class "item"
-    const items = document.querySelectorAll(".item");
-
-    // looping through each element 
-    for (const item of items) {
-
-        // check if input is empty
-        if (item.value.trim() === "") {
-            item.classList.add("error");
-            item.parentElement.classList.add("error");
-        }
-
-        //
-        if(item[1].value != ""){
-            validateEmail();
-        }
-
-        item[1].addEventListener("keyup", () => {
-            validateEmail();
-        })
-
-        // event listener for input changes
-        item.addEventListener("keyup", () => {
-            if (item.value.trim() !== "") {
-                item.classList.remove("error");
-                item.parentElement.classList.remove("error");
-            } else {
-                item.classList.add("error");
-                item.parentElement.classList.add("error");
-            }
-        });
-
-    }
+// function to show error message on an input field
+function showError(input, message) {
+    const errorTxt = input.nextElementSibling.querySelector(".error-msg");
+    errorTxt.textContent = message;
+    first_name.style.border = "1px solid red";
+    errorTxt.parentElement.style.visibility = message ? "visible" : "hidden";
 }
 
-// function to check email validation
-function validateEmail() {
-    const items = document.getElementById("error-txttt");
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    // if 
-    if(!email.value.match(emailRegex)){
-        email.classList.add("error");
-        email.parentElement.classList.add("error");
-        items.textContent = "yoh"
-        
-    }else{
-        email.classList.remove("error");
-        email.parentElement.classList.remove("error");
-    }
+// function to validate user names
+function validateNames(name, userName) {
+    if (name == "") return "" + userName + " can not be empty";
+    if(/[^A-Za-z]/.test(name)) return "" + userName + " must contain only letters";
+    if (name.length < 3) return "" + userName + " must be at least 3 characters long.";
+    return "";
 }
 
-// form event handling 
-document.getElementById("contact-form").addEventListener('submit', (e) => {
+// event on focus listener for user first name
+first_name.addEventListener("focusout", function() {
+    const errorMessage = validateNames(first_name.value, "First Name");
+    showError(first_name, errorMessage);
 
+    // to check the border if inputs are valid
+    if (errorMessage == ""){
+        first_name.style.border = "0.2px solid black";
+    }
+});
+
+// form event handling
+document.getElementById("contact-form").addEventListener("submit", (e) => {
     //prevent default load
     e.preventDefault();
-    checkInputs();
-
 });
